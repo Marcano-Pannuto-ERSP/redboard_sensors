a7d53a87b9802c743cac9a5f41d75731dab49a08
Melody Gill <melodywgill@gmail.com>
2023-07-11 13:28:05 -0700

    Start working on flash read function

--- src/main.c
+++ src/main.c
@@ -16,28 +16,36 @@
 #include <lora.h>
 #include <gpio.h>
 
-#define CHECK_ERRORS(x)\
-	if ((x) != AM_HAL_STATUS_SUCCESS)\
-	{\
-		error_handler(x);\
-	}
-
-static void error_handler(uint32_t error)
+/** Structure representing the flash chip */
+struct flash
 {
-	(void)error;
-	for(;;)
-	{
-		am_devices_led_on(am_bsp_psLEDs, 0);
-		am_util_delay_ms(500);
-		am_devices_led_off(am_bsp_psLEDs, 0);
-		am_util_delay_ms(500);
-	}
+	struct spi *spi;
+};
+
+void flash_init(struct flash *flash, struct spi *spi)
+{
+	flash->spi = spi;
+}
+
+void flash_read_data(struct flash *flash, uint32_t addr, uint32_t *buffer, uint32_t size)
+{
+	// Write the read command
+	// uint8_t toWrite[3];
+	// toWrite[0] = addr >> 16 & 0xFF;
+	// toWrite[1] = addr >> 8 & 0xFF;
+	// toWrite[2] = addr & 0xFF;
+	// spi_write(flash->spi, 0x03, toWrite, 3);
+
+	addr &= 0xFFFFFF;
+	uint32_t toWrite = addr;
+	toWrite += (0x03 << 24);
+	spi_write_continue(flash->spi, &toWrite, 4);
+	spi_read(flash->spi, buffer, size);
 }
 
 static struct uart uart;
-static struct adc adc;
-static struct gpio lora_power;
-static struct lora lora;
+static struct spi spi;
+static struct flash flash;
 
 int main(void)
 {
@@ -52,76 +60,47 @@ int main(void)
 	// Init UART, registers with SDK printf
 	uart_init(&uart, UART_INST0);
 
-	// Initialize the ADC.
-	adc_init(&adc);
-
-	// After init is done, enable interrupts
-	am_hal_interrupt_master_enable();
-
 	// Print the banner.
 	am_util_stdio_terminal_clear();
 	am_util_stdio_printf("Hello World!\r\n\r\n");
 
-	// Trigger the ADC to start collecting data
-	adc_trigger(&adc);
-
-	gpio_init(&lora_power, 10, GPIO_MODE_OUTPUT, false);
+	// gpio_init(&lora_power, 10, GPIO_MODE_OUTPUT, false);
 	//lora_receive_mode(&lora);
 
-	// Wait here for the ISR to grab a buffer of samples.
+	// Initialize spi and select the CS channel
+	spi_init(&spi, 0, 2000000u);
+	spi_chip_select(&spi, SPI_CS_0);
+	spi_enable(&spi);
+
+	// Initialize flash
+	flash_init(&flash, &spi);
+
+	// Test flash functions
+	int size = 10;
+	uint32_t buffer[size];		// this is 4x bigger than necessary
+	for (int i = 0; i < size; i++) {
+		buffer[i] = -1;
+	}
+	// print buffer
+	for (int i = 0; i < size; i++) {
+		am_util_stdio_printf("%d ", buffer[i]);
+		am_util_delay_ms(10);
+	}
+	am_util_stdio_printf("\r\n");
+
+	flash_read_data(&flash, 0x04, buffer, size);
+
+	char* buf = buffer;
+
 	while (1)
 	{
-		// Print the battery voltage and temperature for each interrupt
-		//
-		uint32_t data = 0;
-		if (adc_get_sample(&adc, &data))
-		{
-			// The math here is straight forward: we've asked the ADC to give
-			// us data in 14 bits (max value of 2^14 -1). We also specified the
-			// reference voltage to be 1.5V. A reading of 1.5V would be
-			// translated to the maximum value of 2^14-1. So we divide the
-			// value from the ADC by this maximum, and multiply it by the
-			// reference, which then gives us the actual voltage measured.
-			const double reference = 1.5;
-			double voltage = data * reference / ((1 << 14) - 1);
-
-			double temperature = 5.506 - sqrt((-5.506)*(-5.506) + 4 * 0.00176 * (870.6 - voltage*1000));
-			temperature /= (2 * -.00176);
-			temperature += 30;
-
-			gpio_set(&lora_power, true);
-			// The documentation for the SX1276 states that it takes 10 ms for
-			// the radio to come online from coldboot.
+		// print buffer
+		for (int i = 0; i < size; i++) {
+			am_util_stdio_printf("%02X ", (int)buf[i]);
 			am_util_delay_ms(10);
-			// Only continue if we initialize
-			// FIXME what if we never initialize?
-			while(!lora_init(&lora, 915000000, 42));
-			lora_standby(&lora);
-			lora_set_spreading_factor(&lora, 7);
-			lora_set_coding_rate(&lora, 1);
-			lora_set_bandwidth(&lora, 0x7);
-
-			unsigned char buffer[64];
-			int magnitude = 10000;
-			snprintf((char*)buffer, sizeof(buffer),
-				"{ \"temperature\": %i, \"magnitude\": %i }",
-				(int)(temperature * magnitude),
-				magnitude);
-			int sent = lora_send_packet(&lora, buffer, strlen((char*)buffer));
-			am_util_stdio_printf("length %i %i\r\n", strlen((char*)buffer), sent);
-
-			int received = 0;
-			//while (!(received = lora_receive_packet(&lora, buffer, sizeof(buffer))));
-			if (received)
-			{
-				am_util_stdio_printf("length %i\r\n", received);
-				am_util_stdio_printf("Data: %s\r\n", buffer);
-			}
-			lora_destroy(&lora);
-			gpio_set(&lora_power, false);
 		}
+		am_util_stdio_printf("\r\n");
 
-		// Sleep here until the next ADC interrupt comes along.
-		am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_DEEP);
+		am_util_delay_ms(250);
 	}
 }
